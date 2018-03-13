810cc14b9569336ac3aa859a0d91006e434224d9
tlrkboy <huyinbo@maxthon.net>
2018-03-13 21:06:27 +0800

    完成基本的UI 界面

--- app/src/main/java/com/magic/photouploader/MainActivity.java
+++ app/src/main/java/com/magic/photouploader/MainActivity.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 
 import com.zfdang.multiple_images_selector.ImagesSelectorActivity;
 import com.zfdang.multiple_images_selector.SelectorSettings;
@@ -17,7 +16,6 @@ public class MainActivity extends Activity {
 
     private static final int REQUEST_CODE = 732;
 
-    private TextView tvResults;
     private ArrayList<String> mResults = new ArrayList<>();
 
     @Override
@@ -25,14 +23,14 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button bt = (Button) findViewById(R.id.button);
+        Button bt = (Button) findViewById(R.id.from_album);
         bt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // start multiple photos selector
                 Intent intent = new Intent(MainActivity.this, ImagesSelectorActivity.class);
                 // max number of images to be selected
-                intent.putExtra(SelectorSettings.SELECTOR_MAX_IMAGE_NUMBER, 5);
+                intent.putExtra(SelectorSettings.SELECTOR_MAX_IMAGE_NUMBER, 9);
                 // min size of image which will be shown; to filter tiny images (mainly icons)
                 intent.putExtra(SelectorSettings.SELECTOR_MIN_IMAGE_SIZE, 100000);
                 // show camera or not
@@ -44,7 +42,6 @@ public class MainActivity extends Activity {
             }
         });
 
-        tvResults = (TextView) findViewById(R.id.results);
     }
 
     @Override
@@ -61,7 +58,6 @@ public class MainActivity extends Activity {
                 for(String result : mResults) {
                     sb.append(result).append("\n");
                 }
-                tvResults.setText(sb.toString());
             }
         }
         super.onActivityResult(requestCode, resultCode, data);
